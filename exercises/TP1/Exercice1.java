6a011fd4eade8c4c3602e9c0b8076b791ff46aee
Franz <franzgirardin@yahoo.fr>
2023-05-17 18:29:54 -0400

    JavaDoc

--- exercises/TP1/Exercice1.java
+++ exercises/TP1/Exercice1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Exercice1 {
-	//==================================================================================
+	//=========================================================================================	
     /**
      * Cette fonction prend un tableau de Strings en paramètre et retourne un
      * nouveau tableau contenant les mêmes éléments, avec une case disponible de
@@ -20,18 +20,19 @@ public class Exercice1 {
 	return tabExtd; 
     }
 
-	//==================================================================================
+	//=========================================================================================	
     /**
      * Cette fonction lit des mots sur la ligne de commande et les retourne dans
      * un tableau de Strings. Chaque "mot" est définit comme une ligne complète,
      * du début de la ligne entrée jusqu'au prochain \n.
      */
-    public static String[] lireMots() {
-	//---------------------------------------------------------------------------------	
+	 public static String[] lireMots() {
+	//-----------------------------------------------------------------------------------------	
 	/** On crée une boucle infinie qui permet d'enregistrer les données entrées en ligne
 	 *  de commande dans le tableau. La boucle s'arrête lorsque l'utilisateur écrit
-	 *  "stop" (sans discriminer pour les majuscules).*/
-	//---------------------------------------------------------------------------------	
+	 *  "stop" (sans discriminer pour les majuscules).
+	 */
+	//-----------------------------------------------------------------------------------------	
 		Scanner scan = new Scanner(System.in);
 		String[] motsLus = new String[0];	
 			while (true) {
@@ -48,7 +49,7 @@ public class Exercice1 {
 	return motsLus;
     }
 	
-	//==================================================================================
+	//=========================================================================================	
     /**
      * Cette fonction prend en paramètre un tableau de mots et retourne un
      * nouveau tableau contenant ces mots triés en ordre croissant.
@@ -73,23 +74,23 @@ public class Exercice1 {
     return mots;
     }
 	
-	//==================================================================================
-    /**
-     * Cette fonction prend en paramètre un tableau de mots trié et retourne un
+	//=========================================================================================	  
+	/** 
+	 * Cette fonction prend en paramètre un tableau de mots trié et retourne un
      * nouveau tableau où chaque mot est unique (tous les doublons sont
      * retirés).
      */
     public static String[] retirerDoublons(String[] mots) {
 		// On compte le nombre de mots distincts dans le tableau
 		int nbMotsDistincts = 0;
-	//-----------------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------------------
 	/** 
 	 * On itère sur la longeur du tableau de mots; on compare chaque mot (mots[i])
 	 * à ses sucesseurs (mots[j]). Dès qu'on trouve au moins un sucesseur identique;
 	 * on conclut que le mot est dupliqué et on passe à la comparaison du mot suivant.
 	 * Si le mots n'est identique à aucun de ses sucesseurs; on conclut qu'il est unique
 	 * et incrémente le nombre de mot distinct (nbMotsDisctincts++). */
-	//-----------------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------------------	
 		for (int i = 0; i < mots.length; i++) {
 			boolean estDuplique = false;
 			for (int j = 0; j < i; j++) {
@@ -111,11 +112,11 @@ public class Exercice1 {
 		String[] motsDistincts = new String[nbMotsDistincts];
 
 		int index = 0; // Pour savoir où ajoute un mot unique dans le tab. "motsDistincts"
-		//====================================================================================
+	//-----------------------------------------------------------------------------------------	
 		/* On utilise le même algorithme de comparaison. Cette fois, lorsqu'un mot est unique,
 		 * on l'ajoute au tab. "motsDistincts". S'il est dupliqué, on continue à itérer sur i
 		 * pour trouver le prochain mot unique. */
-		//====================================================================================
+	//-----------------------------------------------------------------------------------------	
 		for (int i = 0; i < mots.length; i++) {
 			boolean estDuplique = false;
 			for (int j = 0; j < i; j++) {
@@ -136,23 +137,12 @@ public class Exercice1 {
     return motsDistincts;
 
     }
-
+	
+	//=========================================================================================
     /**
      * Fonction principale du programme (utilisée pour tester le code).
      */
     public static void main(String[] args) {
-	/*
-	String[] tableau1 = new String[3];
-	tableau1[0] = "Impossible" ; tableau1[1] = "Damn" ;  tableau1[2] = "OK";
-	for (int i = 0 ; i < tableau1.length ; i++) {
-		System.out.println(tableau1[i]);
-	}
-
-	String[] Exttt = agrandirTab(tableau1);
-	for (int i = 0 ; i < Exttt.length ; i++) {
-		System.out.println(Exttt[i]);
-	}
-	*/
 	System.out.println("# Entrée au clavier :");	
 		String[] Help = retirerDoublons(trier(lireMots()));
 	
@@ -166,5 +156,5 @@ public class Exercice1 {
 	}
 
 }
-
+	
 
